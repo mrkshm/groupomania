1e46a27c30359c7251aa2a9d06a9bd7c23cf9f8c
Markus Heumann <mrks.heumann@gmail.com>
2022-04-12 23:49:47 +0200

    fileSaver Things

--- src/utils/fileSaver.ts
+++ src/utils/fileSaver.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 const fileSaver = async (file: any, name: string) => {
   try {
     const data = fs.readFileSync(file.path);
-    fs.writeFileSync(`/public/assets/${name}`, data);
+    fs.writeFileSync(`public/${name}`, data);
     console.log("dirname is", __dirname);
 
     await fs.unlinkSync(file.path);
