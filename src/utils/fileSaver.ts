b7f9eeb3c3f17662a0aa5987caf16f5526a8a68e
Markus Heumann <mrks.heumann@gmail.com>
2022-04-12 22:53:27 +0200

    fileSaver things

--- src/utils/fileSaver.ts
+++ src/utils/fileSaver.ts
@@ -3,12 +3,11 @@ import fs from "fs";
 const fileSaver = async (file: any, name: string) => {
   try {
     const data = fs.readFileSync(file.path);
-    fs.writeFileSync(`./public/assets/${name}`, data);
+    fs.writeFileSync(`./${__dirname}/${name}`, data);
     await fs.unlinkSync(file.path);
     return;
   } catch (error) {
     console.log("There was an error saving the file.");
   }
 };
-
 export default fileSaver;
