64855a1fa797f9e140b4cc9b327733aef77c784e
Markus Heumann <mrks.heumann@gmail.com>
2022-04-12 22:24:55 +0200

    fileSaver changes2

--- src/utils/fileSaver.ts
+++ src/utils/fileSaver.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 const fileSaver = async (file: any, name: string) => {
   try {
     const data = fs.readFileSync(file.path);
-    fs.writeFileSync(`../../public/${name}`, data);
+    fs.writeFileSync(`../../public/assets/${name}`, data);
     await fs.unlinkSync(file.path);
     return;
   } catch (error) {
