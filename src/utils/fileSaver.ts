335a67ec1ef476be9e9dbd03768a1596aaa58947
Markus Heumann <mrks.heumann@gmail.com>
2022-04-13 22:28:23 +0200

    fileSaver back to start

--- src/utils/fileSaver.ts
+++ src/utils/fileSaver.ts
@@ -3,8 +3,7 @@ import fs from "fs";
 const fileSaver = async (file: any, name: string) => {
   try {
     const data = fs.readFileSync(file.path);
-    fs.writeFileSync(`./static/public/${name}`, data);
-    console.log("dirname is", __dirname);
+    fs.writeFileSync(`./public/${name}`, data);
 
     await fs.unlinkSync(file.path);
     return;
