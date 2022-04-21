38d5074b9fb0577cd6221ed2ca5a264e2c7e50bd
Markus Heumann <mrks.heumann@gmail.com>
2022-04-21 16:47:41 +0200

    added google oauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
@@ -37,6 +38,12 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
+    }),
+    GoogleProvider({
+      // @ts-ignore
+      clientId: process.env.GOOGLE_ID,
+      // @ts-ignore
+      clientSecret: process.env.GOOGLE_SECRET
     }),
     EmailProvider({
       server: process.env.EMAIL_SERVER,
