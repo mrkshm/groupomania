14b9a246dbecb3de2d9e976b4020713b504b13c3
Markus Heumann <mrks.heumann@gmail.com>
2022-04-29 20:26:51 +0200

    ts cleanup

--- pages/api/admin/[action]/[userId].ts
+++ pages/api/admin/[action]/[userId].ts
@@ -53,7 +53,7 @@ handler.get(
     if (!session || !session.user) {
       return res.status(401).json({ error: "pas autorisé(e)" });
     }
-
+    // @ts-ignore
     const uId = session.user.id;
     if (!uId) {
       return res.status(401).json({ message: "Pas connecté ?" });
